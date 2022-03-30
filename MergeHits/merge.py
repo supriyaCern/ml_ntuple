b2587df5788287e8c2cd4ae5ce5cd32f9eef4433
macc <macc@maccs-MacBook-Air.local>
2022-03-30 15:15:04 +0530

    added merging codes

---
+++ MergeHits/merge.py
@@ -0,0 +1,59 @@
+import ROOT as rt
+import array as ar
+
+inHE = rt.TFile.Open("/eos/user/p/psuryade/ml_ntuples/21_3/ml_ntuple_HE3.root", "READ")
+inHEB = rt.TFile("/eos/user/p/psuryade/ml_ntuples/21_3/ml_ntuple_HEB3.root", "READ")
+outfile2 = rt.TFile("/eos/user/p/psuryade/ml_ntuples/21_3/ml_ntupleHE2.root", "RECREATE")
+
+nHit = ar.array('i', [0])
+X_ = ar.array('f', 2000*[0.0])
+Y_ = ar.array('f', 2000*[0.0])
+E_ = ar.array('f', 2000*[0.0])
+t_ = ar.array('f', 2000*[0.0])
+adc_ = ar.array('H', 2000*[0])
+thick_ = ar.array('H', 2000*[0])
+intree = []
+intreeB = []
+outtree2 = []
+
+for i in range(34, 48):
+    intree.append(inHE.Get("Events/layer_" + str(i)))
+    intreeB.append(inHEB.Get("Events/layer_" + str(i)))
+    outtree2.append(rt.TTree("layer_" + str(i) , "hits in layer" + str(i)))
+    outtree2[-1].Branch("nHit", nHit, "nHit/I")
+    outtree2[-1].Branch("X", X_, "X[nHit]/F")
+    outtree2[-1].Branch("Y", Y_, "Y[nHit]/F")
+    outtree2[-1].Branch("SimHitE", E_, "SimHitE[nHit]/F")
+    outtree2[-1].Branch("time", t_, "time[nHit]/F")
+    outtree2[-1].Branch("ADC", adc_, "ADC[nHit]/s")
+    outtree2[-1].Branch("Thick", thick_, "Thick[nHit]/s")
+
+for j in range(1000):
+    for i in range(34,48):
+        intree[i-34].GetEntry(j)
+        intreeB[i-34].GetEntry(j)
+        nHitsi = intree[i-34].nHit 
+        nHit[0] = intree[i-34].nHit + intreeB[i-34].nHit
+        for ii in range(nHitsi):
+            X_[ii] = intree[i-34].X[ii]
+            Y_[ii] = intree[i-34].Y[ii]
+            E_[ii] = intree[i-34].SimHitE[ii]
+            t_[ii] = intree[i-34].time[ii]
+            adc_[ii] = intree[i-34].ADC[ii]
+            thick_[ii] = intree[i-34].Thick[ii]
+        for ii in range(intreeB[i-34].nHit):
+            X_[nHitsi + ii] = intreeB[i-34].X[ii]
+            Y_[nHitsi + ii] = intreeB[i-34].Y[ii]
+            E_[nHitsi + ii] = intreeB[i-34].SimHitE[ii]
+            t_[nHitsi + ii] = intreeB[i-34].time[ii]
+            adc_[nHitsi + ii] = intreeB[i-34].ADC[ii]
+            thick_[nHitsi + ii] = intreeB[i-34].Thick[ii]
+        outtree2[i-34].Fill()
+        #print(nHit[0])
+
+tdir = outfile2.mkdir("Events")
+#tdir = rt.TDirectory('Events', 'Events')
+tdir.cd()
+for i in range(34, 48):
+    outtree2[i-34].Write()
+#tdir.Write()
---
+++ MergeHits/split.py
@@ -0,0 +1,49 @@
+import ROOT as rt
+import array as ar
+
+inHE = rt.TFile.Open("ml_ntuple_HE3.root", "READ")
+outfile2 = rt.TFile("ml_ntupleHE1.root", "RECREATE")
+
+nHit = ar.array('i', [0])
+X_ = ar.array('f', 2000*[0.0])
+Y_ = ar.array('f', 2000*[0.0])
+E_ = ar.array('f', 2000*[0.0])
+t_ = ar.array('f', 2000*[0.0])
+adc_ = ar.array('H', 2000*[0])
+thick_ = ar.array('H', 2000*[0])
+intree = []
+intreeB = []
+outtree2 = []
+
+for i in range(27, 34):
+    intree.append(inHE.Get("Events/layer_" + str(i)))
+    outtree2.append(rt.TTree("layer_" + str(i) , "hits in layer" + str(i)))
+    outtree2[-1].Branch("nHit", nHit, "nHit/I")
+    outtree2[-1].Branch("X", X_, "X[nHit]/F")
+    outtree2[-1].Branch("Y", Y_, "Y[nHit]/F")
+    outtree2[-1].Branch("SimHitE", E_, "SimHitE[nHit]/F")
+    outtree2[-1].Branch("time", t_, "time[nHit]/F")
+    outtree2[-1].Branch("ADC", adc_, "ADC[nHit]/s")
+    outtree2[-1].Branch("Thick", thick_, "Thick[nHit]/s")
+
+for j in range(1000):
+    for i in range(27,34):
+        intree[i-27].GetEntry(j)
+        nHitsi = intree[i-27].nHit 
+        nHit[0] = intree[i-27].nHit
+        for ii in range(nHitsi):
+            X_[ii] = intree[i-27].X[ii]
+            Y_[ii] = intree[i-27].Y[ii]
+            E_[ii] = intree[i-27].SimHitE[ii]
+            t_[ii] = intree[i-27].time[ii]
+            adc_[ii] = intree[i-27].ADC[ii]
+            thick_[ii] = intree[i-27].Thick[ii]
+        outtree2[i-27].Fill()
+        #print(nHit[0])
+
+tdir = outfile2.mkdir("Events")
+#tdir = rt.TDirectory('Events', 'Events')
+tdir.cd()
+for i in range(27, 34):
+    outtree2[i-34].Write()
+#tdir.Write()
