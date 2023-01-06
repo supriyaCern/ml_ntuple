d4c5e1f8ba2fca5e9f4ca102eb4e6d45668ee8f0
idas <idas@cern.ch>
2023-01-06 12:15:06 +0100

    Creating file : 1) check_condor-errors_mono.sh, updating 1) list_crash_jobs_full.sh 2) merge_mono.sh

--- condor_2/check_condor-errors_regular.sh
+++ condor_2/check_condor-errors_regular.sh
@@ -1,5 +1,5 @@
 
-signal=muon
+signal=muplus
 
 basedir=/afs/cern.ch/work/i/idas/CMSSW/CMSSW_12_4_2/src/ml_ntuple/condor_2
 for PU in 00 35 70 100 150 200
@@ -11,10 +11,10 @@ do
     cd $basedir
 done
 
-for PU in 70_1 100_1 150_1 200_1
+for PU in 70 100 150 200
 do
     echo ======================= Pileup $PU ================================
-    execdir=$basedir/${signal}${PU}/log
+    execdir=$basedir/${signal}${PU}_1/log
     cd $execdir
     source $basedir/list_crash_jobs_full.sh
     cd $basedir
--- condor_2/merge_mono.sh
+++ condor_2/merge_mono.sh
@@ -1,5 +1,6 @@
 
-signal=muplus
+signal=muon
+signalname=Mu_Minus
 resourcedir=/eos/cms/store/group/dpg_hgcal/comm_hgcal/geomval/ntuples
 outdir=$resourcedir/merged
 for PU in 00 100 200
@@ -8,12 +9,13 @@ do
     do
         for eta in 1.7 1.95 2.2 2.45 2.7
         do
+	    echo ======================= Pileup $PU pT ${pT} eta ${eta} ================================
 	    printf -v zpadPU "%03d" $PU
 	    printf -v zpadpT "%03d" $pT
 	    indir=$resourcedir/${signal}_PU_${PU}_pT_${pT}_eta_${eta}
 	    pre_eta=`echo $eta | cut -d '.' -f 1`
 	    post_eta=`echo $eta | cut -d '.' -f 2`
-	    outfile=$outdir/Raw_Hits_Mono_Mu_Plus_PU_${zpadPU}_Pt_${zpadpT}_Eta_${pre_eta}pt${post_eta}.root
+	    outfile=$outdir/Raw_Hits_Mono_${signalname}_PU_${zpadPU}_Pt_${zpadpT}_Eta_${pre_eta}pt${post_eta}.root
 	    unset array
 	    declare -a array
 	    nof_file=0
@@ -23,12 +25,12 @@ do
 		nof_file=$[$nof_file+1]
 	    done
 	    echo files are ${array[@]}
+	    echo Found nof files : ${nof_file}
 	    echo Output file name $outfile
 	    if [ -f $outfile ] ; then
-		rm $outfile
+	    	rm $outfile
 	    fi
             hadd $outfile ${array[@]}
-            sleep 10
         done
     done
 done
